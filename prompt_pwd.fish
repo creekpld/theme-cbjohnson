32944252661585af1c82f743582de584ca97123c
Philipp Dylong <creekpld@users.noreply.github.com>
2016-09-24 15:50:29 +0200

    fix typo

--- prompt_pwd.fish
+++ prompt_pwd.fish
@@ -1,7 +1,7 @@
-ffunction prompt_pwd --description 'Print the current working directory, NOT shortened to fit the prompt'
+function prompt_pwd --description 'Print the current working directory, NOT shortened to fit the prompt'
  	if test "$PWD" != "$HOME"
-    		printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|")
+ 	    printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|")
  	else
-    		echo '~'
+ 	    echo '~'
  	end
-eend
+end
