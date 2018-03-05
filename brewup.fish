8474984fe64e5a30745ae3a112984b5201fba97c
Philipp Dylong <phd@combase.de>
2018-03-05 08:58:19 +0100

    :tada: added homebrew upgrade function

---
+++ brewup.fish
@@ -0,0 +1,3 @@
+function brewup -d "upgrade and cleanup brew and brew casks"
+	brew update; brew upgrade; brew cask upgrade; brew cleanup; brew cask cleanup;
+end
---
+++ config.fish
@@ -0,0 +1,4 @@
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
+alias java8="/Library/Java/JavaVirtualMachines/jdk1.8.0_161.jdk/Contents/Home/bin/java"
+alias java9="/Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/bin/java"
