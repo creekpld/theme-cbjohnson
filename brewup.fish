c29651d6198a16fe6fb77595ff96234f7f70304c
Philipp Dylong <phd@combase.de>
2018-08-21 10:25:24 +0200

    :heavy_minus_sign: removed deprecated Call `brew cask cleanup`

--- brewup.fish
+++ brewup.fish
@@ -1,3 +1,3 @@
 function brewup -d "upgrade and cleanup brew and brew casks"
-	brew update; brew upgrade; brew cask upgrade; brew cleanup; brew cask cleanup;
+	brew update; brew upgrade; brew cask upgrade; brew cleanup;
 end
